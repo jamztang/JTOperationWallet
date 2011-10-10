7425004f0228c81ed14c795e355be7bdaa79b072
James Tang <mystcolor@gmail.com>
2011-10-10 20:56:32 +0800

    Initial Commit

---
+++ JTOperationWalletTests/JTOperationWalletTests.h
@@ -0,0 +1,13 @@
+//
+//  JTOperationWalletTests.h
+//  JTOperationWalletTests
+//
+//  Created by james on 10/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JTOperationWalletTests : SenTestCase
+
+@end
---
+++ JTOperationWalletTests/JTOperationWalletTests.m
@@ -0,0 +1,32 @@
+//
+//  JTOperationWalletTests.m
+//  JTOperationWalletTests
+//
+//  Created by james on 10/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "JTOperationWalletTests.h"
+
+@implementation JTOperationWalletTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in JTOperationWalletTests");
+}
+
+@end
